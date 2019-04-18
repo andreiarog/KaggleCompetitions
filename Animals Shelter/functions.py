e29034d9c59689a6c79573a63b5c4624e17182d3
andreiarog <andffjr@gmail.com>
2019-04-18 21:51:23 +0100

    morestuff

--- Animals Shelter/functions.py
+++ Animals Shelter/functions.py
@@ -2,6 +2,24 @@
 import pandas as pd
 import numpy as np
 
+def pre_process(dataset):
+
+    df=dataset.copy()       
+    df=process_date_attributes(df)
+    df=process_name(df)
+    df=process_sex(df)
+    df=process_age(df)
+    df=process_breed(df)
+    df=process_colour(df)
+
+    toDummy=["TimeOfDay", "AnimalType"]
+    for column in toDummy:
+        df = create_dummies(df, column)
+    
+    df=df.drop([
+        
+    return df
+
 def column_split(df,column,sep,n,new_col):
 	'''function to create new columns from the original one when 2 or more attributes are stored in the same column, returns the new dataframe'''
 	'''df is the original dataframe, column is the column to split, sep is the separator between attributes, n is the number of new columns, new_col is the list of new column names in the order that the attributes appear in the original column'''
